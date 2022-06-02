0255acc9b7d607559576aeea7f479c11bb8373d9
Dane Jensen <firstlegoboy@comcast.net>
2022-06-02 17:46:18 -0500

    Update dsr_death.py
    Made updates to the dsr sim to improve usablity for other rolls

--- dsr_death.py
+++ dsr_death.py
@@ -1,11 +1,32 @@
+### Drangonsong's Reprise Utimate Death of the Heaven's personal Sim
+###
+### This is intended to help players get more consistent with the death of the heavens mech.
+### This sim was made with the assumption that you are using a stragegy for selecting which player
+### goes where in a straight line similar to this toolbox: https://ff14.toolboxgaming.space/?id=346958196041561&preview=1
+### for the group's strategy for this mechanic. it will calc each mechanic of the phase and tell you
+### which ones are affecting you in the following order:
+###
+### - Warrior location
+### - Doom order (from closest to warrior to furthest from warrior)
+### - What play station marker you have and where the eye / king thordan are
+###
+### To run this on your own machine, you need python 3 installed and the pyttsx3 library installed
+### Usage: python3 dsr_death.py <position>
+
+
+
 import pyttsx3
 import random
 import time
+from sys import argv
+
+## Vairables to change
+position = argv[1] if len(argv) > 1 else 'd4' # this should be your position in the line up for death
+
 
 ## Calc who gets what and where
-position = 'd4'
-l = ['h1', 'd1', 'd2', 'mt', 'ot', 'd3', 'd4', 'h2']
-dooms = random.sample(l, 4)
+lineup = ['h1', 'd1', 'd2', 'mt', 'ot', 'd3', 'd4', 'h2']
+dooms = random.sample(lineup, 4)
 i_am_doomed = 'd4' in dooms
 marker = ''
 doom_markers = ['red', 'yellow', 'purple']
@@ -28,23 +49,26 @@ while abs(eye_loc - king_loc) <= 1 or abs(eye_loc - king_loc) == 7:
 engine = pyttsx3.init()
 
 vl1 = f"Warrior at {locs[warrior_spawn]}"
-vl2 = "You have doom" if i_am_doomed else "No doom"
-vl3 = marker
+vl2 = "Doom Ordering:"
+for x in lineup:
+    vl2 = f"{vl2} Doom, " if x in dooms else f"{vl2} No Doom, "
+vl3 = f"{marker}.  Eye at {locs[eye_loc]}, King at {locs[king_loc]}"
 
 engine.say(vl1)
 engine.runAndWait()
-time.sleep(6)
+time.sleep(4)
 engine.say(vl2)
 engine.runAndWait()
-time.sleep(9)
+time.sleep(6)
 engine.say(vl3)
 engine.runAndWait()
 
 
 ## Print the cheat sheet
 print("CHEATSHEET:")
-print("Warrior at ", locs[warrior_spawn])
-print("Doomed Players ", dooms)
-print("Your Marker ", marker)
+print("Warrior at: ", locs[warrior_spawn])
+print("Your Position: ", position)
+print("Doomed Players: ", dooms)
+print("Your Marker: ", marker)
 print("Eye at ", locs[eye_loc])
 print("King at ", locs[king_loc])
